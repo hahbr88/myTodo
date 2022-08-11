c236ca63cb263c8e3f080bed7a6bd7675f2e1122
hahbr88 <90291796+hahbr88@users.noreply.github.com>
2022-08-11 13:16:42 +0900

    WriteBook.jsx, 링크

--- my_todo/src/pages/WriteBook.jsx
+++ my_todo/src/pages/WriteBook.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { v4 as uuidv4 } from "uuid";
-import { __postBooks,__getBooks } from "../redux/modules/inputSlice";
+import { __postBooks, __getBooks } from "../redux/modules/inputSlice";
 import { useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const WriteBook = () => {
   const [nickName, setNickName] = useState({
@@ -15,30 +17,37 @@ const WriteBook = () => {
     contents: "",
   });
 
-  const userId = uuidv4()
+  const userId = uuidv4();
 
   const dispatch = useDispatch();
 
-  const getbook = useSelector((state)=> state.books.books) 
+  const getbook = useSelector((state) => state.books.books);
 
+  console.log(getbook);
+
+  const { id } = useParams;
+  const navigate = useNavigate;
+
+  console.log(id);
 
   // const nowDT = new Date().toLocaleString();
 
-  useEffect(()=>{
-    dispatch(__getBooks())
-  },[dispatch])  
+  useEffect(() => {
+    dispatch(__getBooks());
+  }, [dispatch]);
 
   const onSubmitHandler = (e) => {
     e.preventDefault();
-    if (title === "" || nickName === "") return false; // 둘중 하나라도 입력하지 않았을 때 dispatch 하지 않음
+    if (title === "" && nickName === "" && content === "") {
+      return false;
+    } // 셋 중 하나라도 입력하지 않았을 때 dispatch 하지 않음
     else {
-      setNickName('')
-      setTitle('')
-      setContent('')
+      setNickName("");
+      setTitle("");
+      setContent("");
     }
-    dispatch(__postBooks({nickName, title, content, userId}));
+    dispatch(__postBooks({ nickName, title, content, userId }));
   };
-
 
   return (
     <>
@@ -75,19 +84,33 @@ const WriteBook = () => {
         />
         <button>글쓰기</button>
       </form>
-      
+
       <div>
-      {getbook?.length > 0 && 
-      getbook.map((e) => (
-        <div key={e.id}>
-          <div>{e.title}</div>
-          <div>{e.name}</div>
-        </div>
-        )
-      )}
+        {getbook?.length > 0 &&
+          getbook.map((e) => (
+            <div key={e.id}>
+              <div>작성자 : {e.name}</div>
+              <Link to={`/Detail/${e.id}`}>제목:{e.title} </Link>
+            </div>
+          ))}
       </div>
     </>
   );
 };
+
+
+// const InputComponent = ({}) => {
+
+//   return (<input
+//     type="text"
+//     name="nickNames"
+//     placeholder="작성자"
+//     value={nickName.nickNames}
+//     onChange={(e) => {
+//       const { value } = e.target;
+//       setNickName({ ...nickName, nickNames: value });
+//     }}
+//   />)
+// }
 
 export default WriteBook;
